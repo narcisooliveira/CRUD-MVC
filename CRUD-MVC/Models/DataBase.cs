4185d0b201c4efdd197c7ed2b5b004a7559fa1ac
Narciso <nrcsfo@gmail.com>
2023-05-16 07:14:45 -0300

    Add project files.

---
+++ CRUD-MVC/Models/DataBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_MVC.Models
+{
+    public class DataBase : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=.\\sqlexpress;Database=CrudMvc;Trusted_Connection=True;Integrated Security=SSPI;TrustServerCertificate=True");
+    }
+}
